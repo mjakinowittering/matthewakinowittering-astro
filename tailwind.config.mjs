4f36c4b2d70e7a7099b2c1e9f25020f3cc50a608
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2024-07-21 21:48:00 +0100

    Removed unused Google Font.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {
-            fontFamily: {
-                deliciousHandrawn: ['"Delicious Handrawn"', ...defaultTheme.fontFamily.sans]
-            }
-        }
+        extend: {}
     },
     plugins: [require('@tailwindcss/typography')]
 }
