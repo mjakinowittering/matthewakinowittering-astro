a8d6804debb000986b9d30bd0ea39bca4ade2e28
Matthew Akino-Wittering <matthew.akinowittering@outlook.com>
2025-09-13 12:54:31 +0100

    Refactoring the interactive island for the current time in post.

---
+++ src/components/home/experience/event/EventDescription.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { calcLengthInYearsAndMonths } from '../../../../lib/utils.ts'
+
+    interface Props {
+        dateFrom: Date
+        dateTo: Date | null | undefined
+    }
+
+    let { dateFrom, dateTo }: Props = $props()
+</script>
+
+{calcLengthInYearsAndMonths(dateFrom, dateTo ?? new Date())}
